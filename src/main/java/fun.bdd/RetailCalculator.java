a4bef026ee3e0f4562390b6895350a515ca11ed7
brian <brian_sjoberg@yahoo.com>
2019-06-05 08:14:02 -0400

    Accepting price as double and int.

--- src/main/java/fun.bdd/RetailCalculator.java
+++ src/main/java/fun.bdd/RetailCalculator.java
@@ -1,5 +1,13 @@
 package fun.bdd;
 
 public class RetailCalculator {
+    double price;
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/test/java/fun/bdd/RetailCalculatorUnitTest.java
+++ src/test/java/fun/bdd/RetailCalculatorUnitTest.java
@@ -1,8 +1,39 @@
 package fun.bdd;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RetailCalculatorUnitTest {
+    RetailCalculator retailCalculator;
+
+    @Before
+    public void setUp() {
+        retailCalculator = new RetailCalculator();
+    }
+
+    @Test
+    public void should_accept_price_as_double() {
+        retailCalculator.setPrice(10.10);
+        Assert.assertEquals(10.1, retailCalculator.getPrice(), 2);
+    }
+
+    @Test
+    public void should_accept_price_as_int() {
+        retailCalculator.setPrice(20);
+        Assert.assertEquals(20.0, retailCalculator.getPrice(),2);
+    }
+
+    @Test
+    public void should_accept_number_of_items_greater_than_zero() {
+
+    }
+
+    @Test
+    public void should_return_order_value_with_two_decimals_or_less() {
+
+    }
+
     @Test
     public void should_not_accept_number_of_items_less_than_1() {
 
