9f66edce0d4a4c601083b90ff8380ad5049c17d6
brian <brian_sjoberg@yahoo.com>
2019-06-04 20:32:51 -0400

    Fixed the test maven command not finding the stepdefs. Just moved the feature file back into the same directory structure but from test/resources to test/resources/fun/bdd

--- src/test/java/fun/bdd/RunCucumberTest.java
+++ src/test/java/fun/bdd/RunCucumberTest.java
@@ -1,14 +1,13 @@
 package fun.bdd;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/fun/bdd",
-        glue = "src/test/java/fun/bdd"
+        plugin = {"pretty"}
 )
 public class RunCucumberTest {
 }
